62d3b623778ee844aa857c87e844153bd2fcb72f
Adrian <adrian@fundary.net>
2023-11-09 11:06:09 -0500

    Initial Commit

---
+++ AnnoyingBadge/AnnoyingBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnoyingBadgeApp.swift
+//  AnnoyingBadge
+//
+//  Created by Ioan Adrian Hancu on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct AnnoyingBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnnoyingBadge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnnoyingBadge
+//
+//  Created by Ioan Adrian Hancu on 2023-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
